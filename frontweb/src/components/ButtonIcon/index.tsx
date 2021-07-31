af741e8a402914b44920e241cdfaec614983911e
Tiago Gomes <tiagogomes187@gmail.com>
2021-07-31 02:09:06 -0300

    Home

---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,16 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+  return (
+    <div className="btn-card">
+      <div className="btn-content-container">
+        <button className="btn">
+          <h6>VER CATÁLOGO</h6>
+        </button>
+        <p>Comece agora a navegar</p>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonIcon;
--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButtonIcon from 'components/ButtonIcon';
 import Navbar from 'components/Navbar';
 
 import './styles.css';
@@ -8,14 +9,20 @@ const Home = () => {
     <>
       <Navbar />
       <div className="home-container">
-        <div className="home-card">
+        <div className="home-card-banner">
           <div className="home-content-container">
             <h1>O carro perfeito para você</h1>
+            <p>
+              Conheça nossos carros e dê mais um passo na realização do seu
+              sonho
+            </p>
           </div>
           <div className="home-image-container">
             <MainImage />
           </div>
         </div>
+
+        <ButtonIcon />
       </div>
     </>
   );
